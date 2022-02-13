c534a8b0ea4faf704cfe19de15e9efb2fd625b61
Bagus Kurnia <baguskurnia1010@gmail.com>
2022-02-13 19:50:25 +0700

    initial commit

---
+++ backend/constants/index.ts
@@ -0,0 +1,5 @@
+// Address of the Whitelist Contract that you deployed
+export const WHITELIST_CONTRACT_ADDRESS =
+  "https://whitelist-dapp-meta.vercel.app/";
+// URL to extract Metadata for a Crypto Dev NFT
+export const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
